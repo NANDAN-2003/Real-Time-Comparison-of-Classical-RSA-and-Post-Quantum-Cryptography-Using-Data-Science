7527e46a5ba5fa902eb6bfcc9144c28b69a42a28
Devanandanan C <nandannand2003@gmail.com>
2026-03-26 23:03:52 +0530

    updated CSVLogger.csv

--- JavaCrypto/logger/CSVLogger.java
+++ JavaCrypto/logger/CSVLogger.java
@@ -1,5 +1,6 @@
 package JavaCrypto.logger;
 
+import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 
@@ -16,16 +17,38 @@ public class CSVLogger {
             int cipherSize,
             String attackResult) {
 
-        try (FileWriter writer = new FileWriter(FILE_NAME, true)) {
+        try {
 
+            File file = new File(FILE_NAME);
+            boolean fileExists = file.exists();
+
+            FileWriter writer = new FileWriter(file, true);
+
+            // Write header if file is new
+            if (!fileExists || file.length() == 0) {
+
+                writer.append("Algorithm,")
+                      .append("KeySize,")
+                      .append("KeyGenerationTime(us),")
+                      .append("EncryptionTime(us),")
+                      .append("DecryptionTime(us),")
+                      .append("CiphertextSize(bytes),")
+                      .append("AttackResult")
+                      .append("\n");
+            }
+
+            // Write row data
             writer.append(algorithm).append(",")
-                    .append(String.valueOf(keySize)).append(",")
-                    .append(String.valueOf(keyGenTime)).append(",")
-                    .append(String.valueOf(encTime)).append(",")
-                    .append(String.valueOf(decTime)).append(",")
-                    .append(String.valueOf(cipherSize)).append(",")
-                    .append(attackResult)
-                    .append("\n");
+                  .append(String.valueOf(keySize)).append(",")
+                  .append(String.valueOf(keyGenTime)).append(",")
+                  .append(String.valueOf(encTime)).append(",")
+                  .append(String.valueOf(decTime)).append(",")
+                  .append(String.valueOf(cipherSize)).append(",")
+                  .append(attackResult)
+                  .append("\n");
+
+            writer.flush();
+            writer.close();
 
         } catch (IOException e) {
             e.printStackTrace();
